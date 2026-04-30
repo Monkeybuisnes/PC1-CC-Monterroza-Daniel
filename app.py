78ed9dba67c297ed63eab9ff7271e7777ffce15b
Daniel Monterroza Sierra <safeword101@gmail.com>
2026-04-29 22:36:16 -0500

    sixth commit

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ DBNAME = st.secrets["postgres"]["DBNAME"]
 # 🔌 CONEXIÓN A SUPABASE
 # =========================================
 conn = psycopg2.connect(
-    host=HOST,
-    database=DBNAME,
-    user=USER,
-    password=PASSWORD,
-    port=PORT
+    HOST = "db.hwqwrpgasrehjreocnls.supabase.co",
+    PORT = "5432",
+    USER = "postgres",
+    DBNAME = "postgres",
+    PASSWORD = "Practica_01"
 )
 
 # =========================================
